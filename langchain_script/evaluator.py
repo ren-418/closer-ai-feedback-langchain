80a503681c0b7cd92c3d80493a3bf46768438f21
ren-418 <mrsteevtvw418@gmail.com>
2025-07-14 05:29:34 +0800

    feat: fix batch script

--- langchain_script/evaluator.py
+++ langchain_script/evaluator.py
@@ -74,8 +74,7 @@ class SalesCallEvaluator:
                             {"role": "user", "content": prompt}
                         ],
                         "temperature": 0.3,
-                        "max_tokens": 2000,
-                        "response_format": {"type": "json_object"}
+                        "max_tokens": 2000
                     }
                 })
             # Write batch file
